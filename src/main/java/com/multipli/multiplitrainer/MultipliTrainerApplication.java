68bc400373a01b94d145cccaecf7c804a7bf4313
Mikhail Trifonov <trifonov2812@gmail.com>
2024-05-04 00:25:11 +0300

    Init Commit

---
+++ src/main/java/com/multipli/multiplitrainer/MultipliTrainerApplication.java
@@ -0,0 +1,12 @@
+package com.multipli.multiplitrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipliTrainerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MultipliTrainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multipli/multiplitrainer/MultipliTrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multipli.multiplitrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipliTrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
